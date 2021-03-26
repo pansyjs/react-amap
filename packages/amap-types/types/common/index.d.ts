c8db5a9b2b17cfca77eef48c8322a0eb917591a2
wangxingkang <wang_xingkang@qq.com>
2021-03-26 14:34:45 +0800

    💥 feat: 添加覆盖物相关类型定义

--- packages/amap-types/types/common/index.d.ts
+++ packages/amap-types/types/common/index.d.ts
@@ -6,11 +6,28 @@ declare namespace AMap {
   type Crs = 'EPSG3857' | 'EPSG3395' | 'EPSG4326';
   type LocationValue = LngLat | [number, number];
 
+  type StrokeLineJoin = 'miter' | 'round' | 'bevel';
+  type StrokeLineCap = 'butt' | 'round' | 'square';
+  type StrokeStyle = 'dashed' | 'solid';
+
+  type AnimationName = 'AMAP_ANIMATION_NONE' | 'AMAP_ANIMATION_DROP' | 'AMAP_ANIMATION_BOUNCE';
+
   type Event<N extends string = string, V = undefined> = { type: N } &
     (V extends HTMLElement ? { value: V }
       : V extends object ? V
       : V extends undefined ? {}
       : { value: V });
+
+  type Omit<T, E extends keyof T> = {
+    [K in Exclude<keyof T, E>]: T[K]
+  };
+
+  type OptionalKey<T> = { [K in keyof T]-?: undefined extends T[K] ? K : never }[keyof T];
+
+  type Merge<O, T> =
+    { [K in Exclude<keyof O, keyof T | OptionalKey<O>>]-?: O[K]; } &
+    { [K in Extract<Exclude<keyof O, keyof T>, OptionalKey<O>>]?: O[K]; } &
+    T;
 
   type MapsEvent<N extends string, I> = Event<N, {
     /**
