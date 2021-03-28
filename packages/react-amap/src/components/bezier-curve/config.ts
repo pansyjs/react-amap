3a6063d03b326989884c32261be7b667c49edb69
wangxingkang <wang_xingkang@qq.com>
2021-03-29 02:05:24 +0800

    💥 feat(react-amap): add BezierCurve component

---
+++ packages/react-amap/src/components/bezier-curve/config.ts
@@ -0,0 +1,16 @@
+export const configurableProps = [
+  /** 动态属性 */
+  'path',
+  'extData',
+  'draggable',
+
+  /* 扩展属性 */
+  'visible',
+  'style'
+]
+
+export const allProps = configurableProps.concat([
+  'zIndex',
+  'bubble',
+  'showDir'
+])
---
+++ packages/react-amap/src/components/bezier-curve/types.ts
@@ -0,0 +1,58 @@
+
+import { AbstractComponentProps } from '../AbstractComponent';
+
+export interface BezierCurveProps extends
+  AMap.BezierCurve.Options,
+  PansyMap.BaseChildrenComponentProps,
+  AbstractComponentProps
+{
+  style?: BezierCurveStyle;
+  visible?: boolean;
+}
+
+interface BezierCurveStyle {
+  /**
+   * 线条是否带描边
+   */
+  isOutline?: boolean;
+  /**
+  * 线条描边颜色
+  */
+  outlineColor?: string;
+  /**
+  * 是否绘制成大地线
+  */
+  geodesic?: boolean;
+  /**
+   * 描边线条颜色
+   */
+  strokeColor?: string;
+  /**
+   * 描边线条透明度
+   */
+  strokeOpacity?: number;
+  /**
+   * 描边宽度
+   */
+  strokeWeight?: number;
+  /**
+   * 描边样式
+   */
+  strokeStyle?: AMap.StrokeStyle;
+  /**
+   * 虚线间隔
+   */
+  strokeDasharray?: number[];
+  /**
+   * 折线拐点的绘制样式
+   */
+  lineJoin?: AMap.StrokeLineJoin;
+  /**
+   * 折线两端线帽的绘制样式
+   */
+  lineCap?: AMap.StrokeLineCap;
+}
+
+export interface BezierCurveState {
+  loaded: boolean
+}
