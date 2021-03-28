a94f628fbb93b7bee72b47d065d0689815ac95b9
wangxingkang <wang_xingkang@qq.com>
2021-03-29 02:01:42 +0800

    💥 feat(react-amap): add Markers component

---
+++ packages/react-amap/src/components/markers/config.ts
@@ -0,0 +1,18 @@
+export { allProps } from '../marker/config';
+
+export const defaultOpts = {
+  useCluster: false,
+  markersCache: [],
+};
+
+export const IdKey = '__react_amap__';
+
+export const ClusterProps = [
+  'gridSize',
+  'minClusterSize',
+  'maxZoom',
+  'averageCenter',
+  'styles',
+  'zoomOnClick',
+  'renderCluserMarker'
+];
---
+++ packages/react-amap/src/components/markers/types.ts
@@ -0,0 +1,37 @@
+import React from 'react';
+import { AbstractComponentProps } from '../AbstractComponent';
+import { MarkerProps as MarkerOptions } from '../marker/types';
+
+/** Marker 标记点的事件对象 */
+export type MarkerEventFunObject = PansyMap.GetEventFunObject<AMap.Marker.EventMap<AMap.Marker>>;
+
+/** MarkerClusterer 聚合对象的事件对象  */
+export type MarkerClustererEventFunObject = PansyMap.GetEventFunObject<AMap.MarkerClusterer.EventMap<AMap.MarkerClusterer>>;
+
+/** Marker 标记点的事件对象 添加创建事件 */
+export interface MarkerEventMap extends MarkerEventFunObject {
+  created?: (clusterer: AMap.Marker[]) => void;
+}
+
+/** MarkerClusterer 聚合对象的事件对象 添加创建事件 */
+export interface MarkerClustererEventMap extends MarkerClustererEventFunObject {
+  created?: (clusterer: AMap.MarkerClusterer) => void;
+}
+
+/** 创建聚合对象的参数 */
+export interface MarkerClustererOptions<ExtraData = any> extends AMap.MarkerClusterer.Options<ExtraData> {
+  events?: MarkerClustererEventMap
+}
+
+export interface MarkersProps<D = any> extends
+  AMap.Marker.Options,
+  PansyMap.BaseChildrenComponentProps,
+  AbstractComponentProps
+{
+  useCluster?: boolean | MarkerClustererOptions<D>;
+  markers?: MarkerOptions[];
+  events?: MarkerEventMap;
+  render?: (options: MarkerOptions<D>) => React.ReactNode | false;
+}
+
+export { MarkerProps as MarkerOptions } from '../marker/types';
