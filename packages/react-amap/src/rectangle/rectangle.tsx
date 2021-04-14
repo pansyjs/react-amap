95f5045736e0a585d3325743bbdab7d76961702f
wangxingkang <wang_xingkang@qq.com>
2021-04-15 00:30:14 +0800

    🚧 wip(Rectangle): 代码优化

--- packages/react-amap/src/rectangle/rectangle.tsx
+++ packages/react-amap/src/rectangle/rectangle.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useImperativeHandle } from 'react';
 import { useMap } from '../map';
 import { usePropsReactive } from '../hooks';
+import { buildCreateOptions, renderEditor } from '../utils/overlay';
 import type { RectangleProps, RectangleType } from './types';
 import { allProps, setterMap, converterMap } from './config';
 
@@ -36,56 +37,23 @@ const Rectangle: RectangleType = (props = {}, ref) => {
   );
 
   const createInstance = () => {
-    const options = buildCreateOptions()
+    const options = buildCreateOptions<RectangleProps, AMap.Rectangle.Options>(
+      props,
+      allProps,
+      converterMap
+    );
     options.map = map;
     instanceObj.current = new window.AMap.Rectangle(options);
     return Promise.resolve();
   }
 
-  const buildCreateOptions = () => {
-    const options: AMap.Rectangle.Options = {}
-    allProps.forEach((key) => {
-      if (key in props) {
-        if (key === 'style' && (props.style !== undefined)) {
-          const styleItem = Object.keys(props.style)
-          styleItem.forEach((item) => {
-            options[item] = props.style[item]
-          })
-        } else if (key !== 'visible') {
-          options[key] = getSetterValue(key, props)
-        }
-      }
-    })
-    return options;
-  }
-
-  /**
-   * 处理需要转换的参数
-   * @param key
-   * @param props
-   * @returns
-   */
-   const getSetterValue = (key: string, props: RectangleProps) => {
-    if (key in converterMap) {
-      return converterMap[key](props[key])
-    }
-    return props[key];
-  }
-
-  const renderEditor = (children: any) => {
-    if (!children) {
-      return null
-    }
-    if (React.Children.count(children) !== 1) {
-      return null
-    }
-    return React.cloneElement(React.Children.only(children), {
-      rectangle: instanceObj.current,
-      map: map
-    })
-  }
-
-  return loaded ? renderEditor(props.children) : null
+  return loaded
+    ? renderEditor<AMap.Rectangle>(props.children, {
+        key: 'rectangle',
+        instance: instanceObj.current,
+        map: map
+      })
+    : null
 }
 
 export default React.forwardRef(Rectangle);
