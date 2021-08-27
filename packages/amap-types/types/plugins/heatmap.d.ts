f61f4723a8485dadd6b104dd5c848eb936f89368
wangxingkang <wang_xingkang@qq.com>
2021-08-27 15:10:24 +0800

    feat(amap-types): 修复 amap 2.0 heatmap 类型问题

--- packages/amap-types/types/plugins/heatmap.d.ts
+++ packages/amap-types/types/plugins/heatmap.d.ts
@@ -2,11 +2,27 @@
 
 declare namespace AMap {
 
-  namespace Heatmap {
+  namespace HeatMap {
     interface EventMap<T extends ShapeOverlay = any> {
       click: MapsEvent<'click', T>;
       mouseover: MapsEvent<'mouseover', T>;
       mouseout: MapsEvent<'mouseout', T>;
+    }
+
+    interface HeatMap3DOptions {
+      /**
+       * 高度缩放因子，表示在单位高度上的缩放比例
+       * @default 1
+       */
+      heightScale?: number;
+      /**
+       * 影响高度平滑度的贝塞尔曲线因子，
+       * @default
+       *   [0.5, 0, 1, 0.5]
+       */
+      heightBezier?: number[];
+      /** 取样精度，越小越平滑，越大性能越高 */
+      gridSize?: number;
     }
 
     interface Options {
@@ -17,33 +33,36 @@ declare namespace AMap {
       radius?: number;
       /**
        * 热力图的渐变区间
+       * 其中 key 表示间隔位置，取值范围： [0,1] ，value 为颜色值
+       * @example
+       * {
+       *   0.4: 'rgb(0, 255, 255)',
+       *   0.85: 'rgb(100, 0, 255)',
+       * }
+       *
        */
-      gradient?: {
-        [key: string]: string
-      };
+      gradient?: Record<number, string>;
       /**
        * 热力图透明度数组，取值范围[0,1]，0表示完全透明，1表示不透明
        * 默认：[0,1]
        */
       opacity?: [number, number];
       /**
-       * 支持的缩放级别范围，取值范围[3-18]
-       * 默认：[3,18]
+       * 支持的缩放级别范围，取值范围[3-20]
+       * @default [3,20]
        */
       zooms?: [number, number];
-
-      rejectMapMask?: boolean;
+      /**
+       * 是否可见
+       */
       visible?: boolean;
-      radiusUnit?: string;
-      blur?: number;
+      /**
+       * 热力图层在地图上的叠加顺序
+       * @default 130
+       */
       zIndex?: number;
-      renderOnZooming?: boolean;
-      ['3d']?: {
-        heightScale?: number;
-        heightBezier?: number[];
-        gridSize?: number;
-        drawGridLine?: boolean;
-      };
+      /** 3D热力图属性 */
+      ['3d']?: HeatMap3DOptions;
     }
 
     interface Data {
@@ -76,39 +95,60 @@ declare namespace AMap {
   /**
    * 热力图，基于第三方heatmap.js实现，以特殊高亮的形式显示数据密集程度。
    */
-  class Heatmap extends AMap.EventEmitter {
+  class HeatMap extends AMap.EventEmitter {
     /**
      * 热力图
      * @param map 地图对象
      * @param opts 热力图选项
      */
-    constructor(map: Map, opts?: Heatmap.Options);
+    constructor(map: Map, opts?: HeatMap.Options);
+
+    /**
+     * 获取热力图叠加地图对象
+     */
+    getMap(): Map;
+
     /**
      * 设置热力图要叠加的地图对象
-     * @param map 地图对象
+     * @param map 地图实例
      */
     setMap(map: Map): void;
+
+    /**
+     * 获取热力图的属性信息
+     */
+    getOptions(): HeatMap.Options;
+
     /**
      * 设置热力图属性
-     * @param opts 热力图属性
+     * @param opts 热力图配置
      */
-    setOptions(opts?: Heatmap.Options): void;
+    setOptions(opts?: HeatMap.Options): void;
+
+
+    /**
+     * 输出热力图的数据集，数据结构同setDataSet中的数据集
+     */
+    getDataSet(): HeatMap.DataSet;
+
     /**
      * 设置热力图展现的数据集，数据格式详见
      * https://lbs.amap.com/api/javascript-api/reference/layer#m_AMap.Heatmap
      * @param dataset 数据集
      */
-    setDataSet(dataset: Heatmap.DataSet | {
-        data: string;
-        dataParser?(data: any): Heatmap.DataSet;
+    setDataSet(dataset: HeatMap.DataSet | {
+      data: string;
+      dataParser?: (data: any) => HeatMap.DataSet;
     }): void;
+
     /**
      * 向热力图数据集中添加坐标点，count不填写时默认：1
-     * @param lng 经度
-     * @param lat 维度
+     * @param longitude 经度
+     * @param latitude 维度
      * @param count 权重
      */
-    addDataPoint(lng: number, lat: number, count?: number): void;
+    addDataPoint(longitude: number, latitude: number, count?: number): void;
+
     /**
      * 隐藏热力图
      */
@@ -117,21 +157,17 @@ declare namespace AMap {
      * 显示热力图
      */
     show(): void;
-    /**
-     * 获取热力图叠加地图对象
-     */
-    getMap(): Map;
-    /**
-     *     获取热力图的属性信息
-     */
-    getOptions(): Heatmap.Options;
-    /**
-     * 输出热力图的数据集，数据结构同setDataSet中的数据集
-     */
-    getDataSet(): Heatmap.DataSet;
 
-    // internal
+    /**
+     * 设置热力图层叠加层级
+     * @param zIndex
+     */
     setzIndex(zIndex: number): void;
+
+    /**
+     * 获取热力图层叠加层级
+     * @param zIndex
+     */
     getzIndex(): number;
   }
 }
