1cd9f6d26eb6e364189b6a4c2f41fb148826df7c
wangxingkang <wang_xingkang@qq.com>
2021-08-29 22:02:21 +0800

    feat(amap-types): 完善类型

--- packages/amap-types/types/common/event.d.ts
+++ packages/amap-types/types/common/event.d.ts
@@ -15,6 +15,7 @@ declare namespace AMap {
       once?: boolean,
       unshift?: boolean
     ): this;
+
     /**
      * 移除事件绑定
      * @param eventName 事件名称
@@ -26,6 +27,25 @@ declare namespace AMap {
       handler: ((this: C, event: any) => void) | 'mv',
       context?: C
     ): this;
+
+    /**
+     * 判断当前实例是否已经绑定了某个事件回调
+     * @param eventName 事件类型
+     * @param handler 事件回调
+     * @param context 事件上下文
+     */
+    hasEvents<C = this>(
+      eventName: string,
+      handler: ((this: C, event: any) => void) | 'mv',
+      context?: C
+    ): boolean;
+
+    /**
+     * 清除当前实例某一类型的全部事件回调
+     * @param type 事件类型，如果此参数为空，清除实例上的所有绑定的事件回调
+     */
+    clearEvents(type?: string): this;
+
     /**
      * 触发事件
      * @param eventName 事件名称
--- packages/amap-types/types/core/size.d.ts
+++ packages/amap-types/types/core/size.d.ts
@@ -9,20 +9,20 @@ declare namespace AMap {
      * @param height 长度像素
      */
     constructor(width: number, height: number);
+
     /**
      * 获得宽度
      */
     getWidth(): number;
+
     /**
      * 获得高度
      */
     getHeight(): number;
+
     /**
      * 以字符串形式返回尺寸大小对象
      */
     toString(): string;
-
-    // internal
-    contains(size: { x: number; y: number }): boolean;
   }
 }
