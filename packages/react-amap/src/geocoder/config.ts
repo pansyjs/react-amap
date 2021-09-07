f2f0ae6ceb8d35de1c2aaa2d24ef646d90a64f17
wangxingkang <wang_xingkang@qq.com>
2021-09-07 11:32:47 +0800

    feat(react-amap): 优化 Geocoder 组件

---
+++ packages/react-amap/src/geocoder/config.ts
@@ -0,0 +1,11 @@
+export const setterMap = {};
+
+export const converterMap = {};
+
+export const allProps = [
+  'city',
+  'radius',
+  'lang',
+  'batch',
+  'extensions'
+]
--- packages/react-amap/src/geocoder/geocoder.tsx
+++ packages/react-amap/src/geocoder/geocoder.tsx
@@ -1,33 +1,67 @@
-import React, { useRef, useState, useEffect, useImperativeHandle } from 'react';
+import React, { useRef, useEffect, useImperativeHandle } from 'react';
+import { usePropsReactive } from '../hooks';
+import { converterMap, setterMap, allProps } from './config';
 import type { GeocoderProps } from './types';
 
-export const Geocoder = React.forwardRef<AMap.Geocoder, GeocoderProps>(({ events, ...rest }, ref) => {
-  const [loaded, setLoaded] = useState<boolean>();
-  const instanceObj = useRef<AMap.Geocoder>(null);
+export const Geocoder = React.forwardRef<
+  AMap.Geocoder,
+  GeocoderProps
+>((props, ref) => {
+  const geocoder = useRef<AMap.Geocoder>(null);
+
+  const { loaded, onInstanceCreated } = usePropsReactive<any, GeocoderProps>(
+    props,
+    geocoder,
+    {
+      setterMap,
+      converterMap
+    }
+  );
 
   useEffect(
     () => {
       createInstance().then(() => {
-        setLoaded(true);
-        events?.created?.(instanceObj.current);
+        onInstanceCreated?.(geocoder.current)
       });
     },
     []
   );
 
+
   useImperativeHandle(
     ref,
-    () => instanceObj.current,
+    () => geocoder.current,
     [loaded]
   );
 
   const createInstance = () => {
     return new Promise<void>((resolve) => {
       AMap.plugin(['AMap.Geocoder'], () => {
-        instanceObj.current = new AMap.Geocoder(rest);
+        const opts = buildCreateOptions();
+
+        geocoder.current = new AMap.Geocoder(opts);
         resolve();
       })
     });
+  }
+
+  const buildCreateOptions = () => {
+    const options: AMap.Geocoder.Options = {};
+
+    const getSetterValue = (key: string, props: GeocoderProps) => {
+      if (key in converterMap) {
+        return converterMap[key](props[key])
+      }
+      return props[key];
+    }
+
+    allProps.forEach((key) => {
+      if (key in props) {
+        options[key] = getSetterValue(key, props)
+      }
+    });
+
+    return options;
   }
 
   return null;
