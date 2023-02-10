cd965ab6401342754c5a1c1b217b0da8aac98066
wangxingkang <wang_xingkang@qq.com>
2023-02-10 12:14:46 +0800

    fix: 修复在 electron 使用资源加载协议问题

--- packages/api-loader/src/utils.ts
+++ packages/api-loader/src/utils.ts
@@ -1,7 +1,7 @@
 import { Config } from './types';
 
 export function getScriptSrc(cfg: Config) {
-  return `//webapi.amap.com/maps?callback=__amap_init_callback` +
+  return `https://webapi.amap.com/maps?callback=__amap_init_callback` +
   `&v=${cfg.AMap.version}` +
   `&key=${cfg.key}` +
   `&plugin=${cfg.AMap.plugins.join(',')}`
